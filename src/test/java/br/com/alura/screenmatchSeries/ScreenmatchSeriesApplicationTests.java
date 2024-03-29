bd38397280e3f77357fd58da28f9990acc59f980
Eduardo Henrique <eduardohcvm@gmail.com>
2024-03-29 13:09:45 -0300

    Initial commit

---
+++ src/test/java/br/com/alura/screenmatchSeries/ScreenmatchSeriesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatchSeries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchSeriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
