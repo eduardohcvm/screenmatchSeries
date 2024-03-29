4fa51c83bb6ffe6ee7cfdd5b1fae9b6a2adece01
Eduardo Henrique <eduardohcvm@gmail.com>
2024-03-29 13:44:11 -0300

    segundo commit spring funcionando !

--- src/main/java/br/com/alura/screenmatchSeries/ScreenmatchSeriesApplication.java
+++ src/main/java/br/com/alura/screenmatchSeries/ScreenmatchSeriesApplication.java
@@ -1,13 +1,19 @@
 package br.com.alura.screenmatchSeries;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchSeriesApplication {
+public class ScreenmatchSeriesApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchSeriesApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// é como se fosse um metodo main
+		System.out.println("projeto spring sem web");
+	}
 }
