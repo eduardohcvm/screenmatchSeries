678ca1d82edc4c07b559e7864cb146b6bd1a5297
Eduardo Henrique <eduardohcvm@gmail.com>
2024-04-01 13:20:06 -0300

    terceiro commit (conexao com OMDB ja funcionando)

--- src/main/java/br/com/alura/screenmatchSeries/ScreenmatchSeriesApplication.java
+++ src/main/java/br/com/alura/screenmatchSeries/ScreenmatchSeriesApplication.java
@@ -1,8 +1,12 @@
 package br.com.alura.screenmatchSeries;
 
+import br.com.alura.screenmatchSeries.services.ConsumoAPI;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.FileWriter;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class ScreenmatchSeriesApplication implements CommandLineRunner {
@@ -14,6 +18,14 @@ public class ScreenmatchSeriesApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// é como se fosse um metodo main
-		System.out.println("projeto spring sem web");
+		FileWriter escrita = new FileWriter("series.txt");
+		Scanner leitura = new Scanner(System.in);
+
+		System.out.println("Digite o nome da serie que voce gostaria de pesquisar: ");
+		var busca = leitura.nextLine();
+		String endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=825f193a";
+		ConsumoAPI api = new ConsumoAPI();
+		String json = api.obterDados(endereco);
+		System.out.println(json);
 	}
 }
