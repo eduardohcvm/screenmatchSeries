372f9e0593cace3ad771db7e596a794172961f44
Eduardo Henrique <eduardohcvm@gmail.com>
2024-04-07 21:00:06 -0300

    sexto commit, pesquisa por temporada e episodio feita

--- src/main/java/br/com/alura/screenmatchSeries/ScreenmatchSeriesApplication.java
+++ src/main/java/br/com/alura/screenmatchSeries/ScreenmatchSeriesApplication.java
@@ -3,6 +3,7 @@ package br.com.alura.screenmatchSeries;
 
 import br.com.alura.screenmatchSeries.model.DadosEpisodios;
 import br.com.alura.screenmatchSeries.model.DadosSerie;
+import br.com.alura.screenmatchSeries.model.DadosTemporada;
 import br.com.alura.screenmatchSeries.services.ConsumoAPI;
 import br.com.alura.screenmatchSeries.services.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
@@ -37,23 +38,14 @@ public class ScreenmatchSeriesApplication implements CommandLineRunner {
 
 		System.out.println("Digite a temporada: ");
 		var temporada = leitura.nextInt();
-		endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&season=" + temporada +"&apikey=825f193a";
-		json = api.obterDados(endereco);
-		System.out.println(json);
-
-		ConverteDados conversor = new ConverteDados(); // cria um conversor de converter dados
-		DadosSerie dados = conversor.obterDados(json,DadosSerie.class); // cria uma instancia chamada dados e aplicar o
-		// conversor chamando o obter dados, passando os parametros para a interface  sendo o json e a classe DadosSerie
-		System.out.println(dados);
-
+		ConverteDados conversor = getConverteDados(busca, temporada, api);
+		buscarTemporada(busca,temporada, api,conversor);
 
 		System.out.println("Digite o episodio em que voce gostaria de analisar: ");
 		var episodio = leitura.nextInt();
-		endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&season=" + temporada + "&episode=" + episodio + "&apikey=825f193a";
-		json = api.obterDados(endereco);
-		System.out.println(json);
-		DadosEpisodios dadosEpisodios = conversor.obterDados(json, DadosEpisodios.class);
-		System.out.println(dadosEpisodios);
+		buscarEpisodio(busca, temporada, episodio, api, conversor);
+
+
 
 		/*
 		// API de fotos de cafe aleatorias
@@ -62,6 +54,39 @@ public class ScreenmatchSeriesApplication implements CommandLineRunner {
 		*/
 
 
+	}
+
+	private static void buscarEpisodio(String busca, int temporada, int episodio, ConsumoAPI api, ConverteDados conversor) {
+		String endereco;
+		String json;
+		endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&season=" + temporada + "&episode=" + episodio + "&apikey=825f193a";
+		json = api.obterDados(endereco);
+		System.out.println(json);
+		DadosEpisodios dadosEpisodios = conversor.obterDados(json, DadosEpisodios.class);
+		System.out.println(dadosEpisodios);
+	}
+	private static void buscarTemporada(String busca, int temporada, ConsumoAPI api, ConverteDados conversor) {
+		String endereco;
+		String json;
+		endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&season=" + temporada + "&apikey=825f193a";
+		json = api.obterDados(endereco);
+		System.out.println(json);
+		DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+		System.out.println(dadosTemporada);
+	}
+
+	private static ConverteDados getConverteDados(String busca, int temporada, ConsumoAPI api) {
+		String json;
+		String endereco;
+		endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&season=" + temporada +"&apikey=825f193a";
+		json = api.obterDados(endereco);
+		System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados(); // cria um conversor de converter dados
+		DadosSerie dados = conversor.obterDados(json,DadosSerie.class); // cria uma instancia chamada dados e aplicar o
+		// conversor chamando o obter dados, passando os parametros para a interface  sendo o json e a classe DadosSerie
+		System.out.println(dados);
+		return conversor;
 	}
 
 }
---
+++ src/main/java/br/com/alura/screenmatchSeries/model/DadosTemporada.java
@@ -0,0 +1,11 @@
+package br.com.alura.screenmatchSeries.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosTemporada(@JsonAlias("Season") Integer temporada,
+                             @JsonAlias("Episodes")List<DadosEpisodios> episodios) {
+}
