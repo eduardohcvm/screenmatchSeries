9ac32475b361cd1e900e0c75f313f24bd7622458
Eduardo Henrique <eduardohcvm@gmail.com>
2024-04-09 22:47:40 -0300

    setimo commit, classe menu e mais organizado

--- src/main/java/br/com/alura/screenmatchSeries/ScreenmatchSeriesApplication.java
+++ src/main/java/br/com/alura/screenmatchSeries/ScreenmatchSeriesApplication.java
@@ -6,12 +6,15 @@ import br.com.alura.screenmatchSeries.model.DadosSerie;
 import br.com.alura.screenmatchSeries.model.DadosTemporada;
 import br.com.alura.screenmatchSeries.services.ConsumoAPI;
 import br.com.alura.screenmatchSeries.services.ConverteDados;
+import br.com.alura.screenmatchSeries.services.ExibirMenu;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.FileWriter;
 import java.util.Scanner;
+
+import static br.com.alura.screenmatchSeries.services.ExibirMenu.buscarEpisodio;
 
 @SpringBootApplication
 public class ScreenmatchSeriesApplication implements CommandLineRunner {
@@ -22,6 +25,7 @@ public class ScreenmatchSeriesApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		ExibirMenu exibirMenu = new ExibirMenu();
 		// é como se fosse um metodo main
 		FileWriter escrita = new FileWriter("series.txt");
 		Scanner leitura = new Scanner(System.in);
@@ -38,12 +42,12 @@ public class ScreenmatchSeriesApplication implements CommandLineRunner {
 
 		System.out.println("Digite a temporada: ");
 		var temporada = leitura.nextInt();
-		ConverteDados conversor = getConverteDados(busca, temporada, api);
-		buscarTemporada(busca,temporada, api,conversor);
+		ConverteDados conversor = exibirMenu.getConverteDados(busca, temporada, api);
+		exibirMenu.buscarTemporada(busca,temporada, api,conversor);
 
 		System.out.println("Digite o episodio em que voce gostaria de analisar: ");
 		var episodio = leitura.nextInt();
-		buscarEpisodio(busca, temporada, episodio, api, conversor);
+	  	exibirMenu.buscarEpisodio(busca, temporada, episodio, api, conversor);
 
 
 
@@ -56,37 +60,6 @@ public class ScreenmatchSeriesApplication implements CommandLineRunner {
 
 	}
 
-	private static void buscarEpisodio(String busca, int temporada, int episodio, ConsumoAPI api, ConverteDados conversor) {
-		String endereco;
-		String json;
-		endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&season=" + temporada + "&episode=" + episodio + "&apikey=825f193a";
-		json = api.obterDados(endereco);
-		System.out.println(json);
-		DadosEpisodios dadosEpisodios = conversor.obterDados(json, DadosEpisodios.class);
-		System.out.println(dadosEpisodios);
-	}
-	private static void buscarTemporada(String busca, int temporada, ConsumoAPI api, ConverteDados conversor) {
-		String endereco;
-		String json;
-		endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&season=" + temporada + "&apikey=825f193a";
-		json = api.obterDados(endereco);
-		System.out.println(json);
-		DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-		System.out.println(dadosTemporada);
-	}
 
-	private static ConverteDados getConverteDados(String busca, int temporada, ConsumoAPI api) {
-		String json;
-		String endereco;
-		endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&season=" + temporada +"&apikey=825f193a";
-		json = api.obterDados(endereco);
-		System.out.println(json);
-
-		ConverteDados conversor = new ConverteDados(); // cria um conversor de converter dados
-		DadosSerie dados = conversor.obterDados(json,DadosSerie.class); // cria uma instancia chamada dados e aplicar o
-		// conversor chamando o obter dados, passando os parametros para a interface  sendo o json e a classe DadosSerie
-		System.out.println(dados);
-		return conversor;
-	}
 
 }
---
+++ src/main/java/br/com/alura/screenmatchSeries/services/ExibirMenu.java
@@ -0,0 +1,39 @@
+package br.com.alura.screenmatchSeries.services;
+
+import br.com.alura.screenmatchSeries.model.DadosEpisodios;
+import br.com.alura.screenmatchSeries.model.DadosSerie;
+import br.com.alura.screenmatchSeries.model.DadosTemporada;
+
+public class ExibirMenu {
+    public static void buscarEpisodio(String busca, int temporada, int episodio, ConsumoAPI api, ConverteDados conversor) {
+        String endereco;
+        String json;
+        endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&season=" + temporada + "&episode=" + episodio + "&apikey=825f193a";
+        json = api.obterDados(endereco);
+
+        DadosEpisodios dadosEpisodios = conversor.obterDados(json, DadosEpisodios.class);
+        System.out.println(dadosEpisodios);
+    }
+    public static void buscarTemporada(String busca, int temporada, ConsumoAPI api, ConverteDados conversor) {
+        String endereco;
+        String json;
+        endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&season=" + temporada + "&apikey=825f193a";
+        json = api.obterDados(endereco);
+        System.out.println(json);
+        DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+        System.out.println(dadosTemporada);
+    }
+
+    public static ConverteDados getConverteDados(String busca, int temporada, ConsumoAPI api) {
+        String json;
+        String endereco;
+        endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&season=" + temporada +"&apikey=825f193a";
+        json = api.obterDados(endereco);
+
+        ConverteDados conversor = new ConverteDados(); // cria um conversor de converter dados
+        DadosSerie dados = conversor.obterDados(json,DadosSerie.class); // cria uma instancia chamada dados e aplicar o
+        // conversor chamando o obter dados, passando os parametros para a interface  sendo o json e a classe DadosSerie
+        System.out.println(dados);
+        return conversor;
+    }
+}
